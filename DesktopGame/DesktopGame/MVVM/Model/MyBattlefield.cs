b0da6c0bcc421239b15741a80ec62195147be518
PortedUser <144102539+PortedUser@users.noreply.github.com>
2023-10-21 14:49:39 +0300

    Ship Set
    Провести рефакторинг, добавить удаление корабля

--- DesktopGame/DesktopGame/MVVM/Model/MyBattlefield.cs
+++ DesktopGame/DesktopGame/MVVM/Model/MyBattlefield.cs
@@ -17,13 +17,12 @@ namespace DesktopGame.MVVM.Model
         private StateCell _baseStateCell;
         public MyBattlefield() : base()
         {
-            
 
             foreach (var cm in Commands)
             {
                 cm.Command = new RelayCommand(o =>
                 {
-                    var state = _parentVM.GetLastSetState();
+                    var state = GetLastSetState();
 
                     if (state.CurrentType == TypeShip.BowShip)
                     {
@@ -49,12 +48,12 @@ namespace DesktopGame.MVVM.Model
         {
             var x = cm.X;
             var y = cm.Y;
-            if (y + 1 < 10 && angle == 0)
+            if (y + 1 < 10 && angle == 0 && CheckSpace(0,1,x,y))
             {
                 this[x, y].SetFullState(StateCell.Deck2_1);
                 this[x , y + 1 ].SetFullState(StateCell.Deck2_2);
             }
-            else if (x + 1 < 10)
+            else if (x + 1 < 10 && CheckSpace(1,0,x,y) && angle == 90)
             {
                 this[x, y].SetFullState(StateCell.Deck2_1_90);
                 this[x + 1, y].SetFullState(StateCell.Deck2_2_90);
@@ -65,12 +64,12 @@ namespace DesktopGame.MVVM.Model
         {
             var x = cm.X;
             var y = cm.Y;
-            if (y + 2 < 10 && angle == 0)
+            if (y + 2 < 10 && angle == 0 && CheckSpace(0,2,x,y))
             {
                 this[x, y].SetFullState(StateCell.Deck3_1);
                 this[x, y + 1].SetFullState(StateCell.Deck3_2);
                 this[x, y + 2].SetFullState(StateCell.Deck3_3);
-            }else if (x + 2 < 10)
+            }else if (x + 2 < 10 && CheckSpace(2,0,x,y) && angle == 90)
             {
                 this[x, y].SetFullState(StateCell.Deck3_1_90);
                 this[x + 1, y].SetFullState(StateCell.Deck3_2_90);
@@ -82,13 +81,13 @@ namespace DesktopGame.MVVM.Model
         {
             var x = cm.X;
             var y = cm.Y;
-            if (y + 3 < 10 && angle == 0)
+            if (y + 3 < 10 && angle == 0 && CheckSpace(0, 3, x, y))
             {
                 this[x, y].SetFullState(StateCell.Deck4_1);
                 this[x, y + 1].SetFullState(StateCell.Deck4_2);
                 this[x, y + 2].SetFullState(StateCell.Deck4_3);
                 this[x, y + 3].SetFullState(StateCell.Deck4_4);
-            }else if(x + 3 < 10)
+            }else if(x + 3 < 10 && CheckSpace(3, 0, x, y) && angle == 90)
             {
                 this[x, y].SetFullState(StateCell.Deck4_1_90);
                 this[x + 1, y].SetFullState(StateCell.Deck4_2_90);
@@ -102,7 +101,7 @@ namespace DesktopGame.MVVM.Model
             if (angle == 0)
             {
                 this[cm.X, cm.Y].SetFullState(StateCell.BowShip);
-            }else if (true)
+            }else if ( angle == 90)
             {
                 this[cm.X, cm.Y].SetFullState(StateCell.BowShip_90);
             }
@@ -127,6 +126,44 @@ namespace DesktopGame.MVVM.Model
         private StateShip GetLastSetState()
         {
             return _parentVM.GetLastSetState();
+        }
+
+
+        private bool CheckSpace(int shiftX, int shiftY, int x, int y)
+        {
+            shiftX += x;
+            shiftY += y;
+            for (var iX = x; iX <= shiftX; iX++)
+            {
+                for (var iY = y; iY <= shiftY; iY++)
+                {
+                    var res = CheckArea(iX, iY);
+                    if (!res)
+                        return res;
+                }
+            }
+            return true;
+        }
+
+        private bool CheckArea(int x, int y)
+        {
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    var X = x + i;
+                    var Y = y + j;
+                    if (X < 10 && Y < 10 && X > -1 && Y > -1)
+                    {
+                        if (this[X, Y].CurrentState != _baseStateCell)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+
         }
     }
 }
